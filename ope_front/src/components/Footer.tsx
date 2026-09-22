eeecf8e43c02c159a110434c441a4c5446e675e5
alk096 <mahamsdoualker@gmail.com>
2026-09-22 17:23:22 +0100

    remove iman logo footer

--- ope_front/src/components/Footer.tsx
+++ ope_front/src/components/Footer.tsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import logoOpe from "../assets/logo_ope.png";
 import logoEvent from "../assets/logo_event.png";
-import logoIman from "../assets/logo_iman.png";
 
 export default function Footer() {
     const { t } = useTranslation();
